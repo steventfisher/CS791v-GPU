9dddfc1a86fa8812a816023d157456eb97ef37ab
stevenfisher <stevenfisher@nevada.unr.edu>
2018-02-01 12:32:03 -0800

    Modified PA0 to run with unified shared memory instead of using the cudaMemCpy

--- PA0-B/src/hellocuda.cu
+++ PA0-B/src/hellocuda.cu
@@ -15,39 +15,11 @@
 int main() {
   
   // Arrays on the host (CPU)
-  int a[N], b[N], c[N];
+  int *a, *b, *c;
   
-  /*
-    These will point to memory on the GPU - notice the correspondence
-    between these pointers and the arrays declared above.
-   */
-  int *dev_a, *dev_b, *dev_c;
-
-  /*
-    These calls allocate memory on the GPU (also called the
-    device). This is similar to C's malloc, except that instead of
-    directly returning a pointer to the allocated memory, cudaMalloc
-    returns the pointer through its first argument, which must be a
-    void**. The second argument is the number of bytes we want to
-    allocate.
-
-    NB: the return value of cudaMalloc (like most cuda functions) is
-    an error code. Strictly speaking, we should check this value and
-    perform error handling if anything went wrong. We do this for the
-    first call to cudaMalloc so you can see what it looks like, but
-    for all other function calls we just point out that you should do
-    error checking.
-
-    Actually, a good idea would be to wrap this error checking in a
-    function or macro, which is what the Cuda By Example book does.
-   */
-  cudaError_t err = cudaMalloc( (void**) &dev_a, N * sizeof(int));
-  if (err != cudaSuccess) {
-    std::cerr << "Error: " << cudaGetErrorString(err) << std::endl;
-    exit(1);
-  }
-  cudaMalloc( (void**) &dev_b, N * sizeof(int));
-  cudaMalloc( (void**) &dev_c, N * sizeof(int));
+  cudaMallocManaged( (void**) &a, N * sizeof(int));
+  cudaMallocManaged( (void**) &b, N * sizeof(int));
+  cudaMallocManaged( (void**) &c, N * sizeof(int));
 
   // These lines just fill the host arrays with some data so we can do
   // something interesting. Well, so we can add two arrays.
@@ -56,38 +28,6 @@ int main() {
     b[i] = i;
   }
 
-  /*
-    The following code is responsible for handling timing for code
-    that executes on the GPU. The cuda approach to this problem uses
-    events. For timing purposes, an event is essentially a point in
-    time. We create events for the beginning and end points of the
-    process we want to time. When we want to start timing, we call
-    cudaEventRecord.
-
-    In this case, we want to record the time it takes to transfer data
-    to the GPU, perform some computations, and transfer data back.
-  */
-  cudaEvent_t start, end;
-  cudaEventCreate(&start);
-  cudaEventCreate(&end);
-
-  cudaEventRecord( start, 0 );
-
-  /*
-    Once we have host arrays containing data and we have allocated
-    memory on the GPU, we have to transfer data from the host to the
-    device. Again, notice the similarity to C's memcpy function.
-
-    The first argument is the destination of the copy - in this case a
-    pointer to memory allocated on the device. The second argument is
-    the source of the copy. The third argument is the number of bytes
-    we want to copy. The last argument is a constant that tells
-    cudaMemcpy the direction of the transfer.
-   */
-  cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_c, c, N * sizeof(int), cudaMemcpyHostToDevice);
-  
   /*
     FINALLY we get to run some code on the GPU. At this point, if you
     haven't looked at add.cu (in this folder), you should. The
@@ -106,33 +46,8 @@ int main() {
     different piece of data, and many threads can execute at the same
     time. This is how cuda can get such large speedups.
    */
-  add<<<N, 1>>>(dev_a, dev_b, dev_c);
-
-  /*
-    Unfortunately, the GPU is to some extent a black box. In order to
-    print the results of our call to add, we have to transfer the data
-    back to the host. We do that with a call to cudaMemcpy, which is
-    just like the cudaMemcpy calls above, except that the direction of
-    the transfer (given by the last argument) is reversed. In a real
-    program we would want to check the error code returned by this
-    function.
-  */
-  cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
-
-  /*
-    This is the other end of the timing process. We record an event,
-    synchronize on it, and then figure out the difference in time
-    between the start and the stop.
-
-    We have to call cudaEventSynchronize before we can safely _read_
-    the value of the stop event. This is because the GPU may not have
-    actually written to the event until all other work has finished.
-   */
-  cudaEventRecord( end, 0 );
-  cudaEventSynchronize( end );
-
-  float elapsedTime;
-  cudaEventElapsedTime( &elapsedTime, start, end );
+  add<<<N, 1>>>(a, b, c);
+  cudaDeviceSynchronize();
 
   /*
     Let's check that the results are what we expect.
@@ -141,15 +56,11 @@ int main() {
     if (c[i] != a[i] + b[i]) {
       std::cerr << "Oh no! Something went wrong. You should check your cuda install and your GPU. :(" << std::endl;
 
-      // clean up events - we should check for error codes here.
-      cudaEventDestroy( start );
-      cudaEventDestroy( end );
-
       // clean up device pointers - just like free in C. We don't have
       // to check error codes for this one.
-      cudaFree(dev_a);
-      cudaFree(dev_b);
-      cudaFree(dev_c);
+      cudaFree(a);
+      cudaFree(b);
+      cudaFree(c);
       exit(1);
     }
   }
@@ -159,14 +70,10 @@ int main() {
     some information about the times we recorded above.
    */
   std::cout << "Yay! Your program's results are correct." << std::endl;
-  std::cout << "Your program took: " << elapsedTime << " ms." << std::endl;
   
   // Cleanup in the event of success.
-  cudaEventDestroy( start );
-  cudaEventDestroy( end );
-
-  cudaFree(dev_a);
-  cudaFree(dev_b);
-  cudaFree(dev_c);
+  cudaFree(a);
+  cudaFree(b);
+  cudaFree(c);
 
 }
